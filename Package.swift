6f64432689d0babd89d36991df9ebf7ce041545b
Bo Gosmer <bo.gosmer@wedobooks.io>
2026-06-19 10:44:21 +0200

    Using v1.3.0 of firebase interfaces package

--- Package.swift
+++ Package.swift
@@ -18,8 +18,10 @@ let package = Package(
             url: "https://github.com/firebase/firebase-ios-sdk.git",
             exact: "12.14.0"
         ),
-        // DEV OVERRIDE: local source path until 1.3.0 xcframework is published; revert to url+exact at publish.
-        .package(path: "../wedobooks-sdk-ios-firebase-interfaces")
+        .package(
+            url: "https://github.com/wedobooks/wedobooks-sdk-ios-firebase-interfaces.git",
+            exact: "1.3.0"
+        )
     ],
     targets: [
         .target(
